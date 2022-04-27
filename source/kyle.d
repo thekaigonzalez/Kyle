a7bd7794882657df86fe191fb456938b26f1bda7
Kai Gonzalez <gkai70264@gmail.com>
2022-04-27 02:31:41 -0400

    Revamp kyle.d

--- source/kyle.d
+++ source/kyle.d
@@ -1,9 +1,19 @@
 module kyle;
 
-public import cv;
-public import kt;
-public import ku;
-public import va;
+public import cp;
 public import kobj;
-public import kc;
+public import ct;
+public import cv;
 public import dg;
+public import ec;
+public import exec;
+public import kc;
+public import ku;
+public import kt;
+public import so;
+public import ss;
+public import st;
+public import va;
+public import vl;
+public import xp;
+public import xps;
